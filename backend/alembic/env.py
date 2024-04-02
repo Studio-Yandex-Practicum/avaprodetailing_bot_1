0ae5ec2ae08b0b8ba978f8b7fb0752bd25e16ce7
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-04-02 10:44:52 +0300

    edited alembic added module models planning flask for forms

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.core.base import Base
+from bot.models import Base
 from app.core.external_functions import check_work_mode
 
 load_dotenv('.env')
@@ -15,7 +15,7 @@ load_dotenv('.env')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', check_work_mode('TEST_MODE'))
+config.set_main_option('sqlalchemy.url', 'sqlite+aiosqlite:///./ava.db')#check_work_mode('TEST_MODE'))
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:

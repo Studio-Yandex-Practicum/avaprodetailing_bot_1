98906ec8a42124ac07ac79b7f69a996e0d541b82
Никита Корняков <nonamebroski@yandex.ru>
2024-04-04 23:14:01 +0300

    Add Car model and alembic migration.

--- backend/app/models/cars.py
+++ backend/app/models/cars.py
@@ -1,12 +1,26 @@
+# from typing import Optional
+
+from sqlalchemy import Column, String  # ForeignKey,
+from sqlalchemy.orm import relationship
+
 from app.core.db import Base
-from sqlalchemy import Column, ForeignKey, Integer, String
 
 
 class Car(Base):
-    id: int = Column(Integer, primary_key=True)
     brand: str = Column(String, nullable=False)
     model: str = Column(String, nullable=False)
-    number_plate: str = Column(String, nullable=False)
-    owner_id: str = Column(
-        String, ForeignKey('user.id', name='fk_car_user_id_owner')
-    )
+    number_plate: str = Column(String, nullable=False, unique=True)
+    # owner_telegram_id: Optional[str] = Column(
+    #     String,
+    #     ForeignKey('user.telegram_id', name='fk_car_user_telegram_id_owner'),
+    #     nullable=True,
+    # )
+    # owner = relationship('User', back_populates='cars')
+
+    def __repr__(self):
+        return (
+            f'{type(self).__name__}('
+            f'brand={self.brand}, model={self.model}, '
+            f'number_plate{self.number_plate})'
+            # , owner_id={self.owner_telegram_id})'
+        )

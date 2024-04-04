46029dd18a9754e4c0985485fd2917804a6e6c6d
Elena Lambert <elena_lambert@yahoo.com>
2024-04-04 16:22:48 +0300

    Add workflow

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,3 +2,8 @@ from fastapi import FastAPI
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_title)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Проверка сервера"}

cf0a7c7510ba0034205864e9c8720fae1de2495d
Никита Корняков <nonamebroski@yandex.ru>
2024-04-04 23:24:49 +0300

    Add main router and connect with cars and users routers. Include main router in app.

--- backend/app/api/routers.py
+++ backend/app/api/routers.py
@@ -1,9 +1,14 @@
-from app.api.endpoints import cars_router
 from fastapi import APIRouter
 
+from app.api.endpoints import cars_router, users_router
+
 CARS_PREFIX = '/cars'
+USERS_PREFIX = '/users'
 
 main_router = APIRouter()
 main_router.include_router(
     cars_router, prefix=CARS_PREFIX, tags=['Автомобили']
+)
+main_router.include_router(
+    users_router, prefix=USERS_PREFIX, tags=['Пользователи']
 )

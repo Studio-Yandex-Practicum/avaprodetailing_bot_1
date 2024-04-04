d8ded6ea3405296b517046fd3204483cf3a36333
Никита Корняков <nonamebroski@yandex.ru>
2024-04-04 23:17:08 +0300

    Add CRUD for Car model and also for future User model with extra methods.

---
+++ backend/app/crud/base.py
@@ -0,0 +1,60 @@
+# from typing import Union
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import Car  # , User
+
+
+class CRUDBase:
+
+    def __init__(self, model):
+        # Изменить по мере добавления других моделей
+        # self.model: Union[Car, User, PoS и тд] = model
+        self.model: Car = model
+
+    async def get(self, object_id: int, session: AsyncSession):
+        return (
+            (
+                await session.execute(
+                    select(self.model).where(self.model.id == object_id)
+                )
+            )
+            .scalars()
+            .first()
+        )
+
+    async def get_all(self, session: AsyncSession):
+        return (
+            (await session.execute(select(self.model).order_by(self.model.id)))
+            .scalars()
+            .all()
+        )
+
+    async def create(
+        self,
+        data,
+        session: AsyncSession,
+    ):
+        new_object_data = data.dict()
+        new_object = self.model(**new_object_data)
+        session.add(new_object)
+        await session.commit()
+        await session.refresh(new_object)
+        return new_object
+
+    async def update(self, object, new_data, session: AsyncSession):
+        update_data = new_data.dict(exclude_unset=True)
+        for field in jsonable_encoder(object):
+            if field in update_data:
+                setattr(object, field, update_data[field])
+        session.add(object)
+        await session.commit()
+        await session.refresh(object)
+        return object
+
+    async def remove(self, object, session: AsyncSession):
+        await session.delete(object)
+        await session.commit()
+        return object
---
+++ backend/app/crud/user.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+
+# from app.models import User
+
+
+class CRUDUser(CRUDBase):
+    async def get_user_by_telegram_id(
+        self, telegram_id: str, session: AsyncSession
+    ) -> Optional[int]:
+        return (
+            (
+                await session.execute(
+                    select(User).where(User.telegram_id == telegram_id)
+                )
+            )
+            .scalars()
+            .first()
+        )
+
+    async def get_user_by_phone_number(
+        self, phone_number: str, session: AsyncSession
+    ) -> Optional[int]:
+        return (
+            (
+                await session.execute(
+                    select(User).where(User.phone_number == phone_number)
+                )
+            )
+            .scalars()
+            .first()
+        )
+
+
+user_crud = CRUDUser(User)

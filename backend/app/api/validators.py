dd3f4c6798cad5c130893f0b5ffa8ae4151a8c7a
Никита Корняков <nonamebroski@yandex.ru>
2024-04-05 16:43:26 +0300

    Improve validators.

--- backend/app/api/validators.py
+++ backend/app/api/validators.py
@@ -4,6 +4,17 @@ from http import HTTPStatus
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.config import (
+    ALLOWED_CHARS,
+    LENGTH_NUMBER_PLATE_ERROR,
+    MAX_LENGTH_BRAND_MODEL,
+    MAX_LENGTH_BRAND_MODEL_ERROR,
+    MAX_LENGTH_NUMBER_PLATE,
+    MIN_LENGTH_BRAND_MODEL_ERROR,
+    MIN_LENGTH_NUMBER_PLATE,
+    MIN_LENGTH_STR,
+    NUMBER_PLATE_FORMAT_ERROR,
+)
 from app.crud.cars import cars_crud
 from app.crud.user import user_crud
 from app.models import Car
@@ -15,41 +26,21 @@ CAR_WITH_NUMBER_EXISTS = 'Автомобиль с таким номером уж
 USER_NOT_FOUND = 'Пользователь не найден.'
 
 NOT_ADMIN_OR_SUPERUSER = 'Действие доступно только администратору.'
-FOREIGN_CAR = 'Вы не можете изменить или удалить чужой автомобиль.'
-LAST_ONE_CAR = (
-    'Последний автомобиль нельзя удалить. Обратитесь к администратору.'
-)
-
-ALLOWED_CHARS = 'АВЕКМНОРСТУХ'
-LENGTH_NUMBER_PLATE = 'Длина номера автомобиля должна быть 8-9 символов.'
-NUMBER_PLATE_FORMAT_ERROR = (
-    'Неверный формат номера. '
-    'Номер нужно вводить в формате А000АА00. Используйте кириллицу.'
-)
-
-ERROR_LENGTH_BRAND_MODEL = 'Поле не может быть пустым.'
-MIN_LENGTH_STR = 1
-MIN_LENGTH_NUMBER_PLATE = 8
-MAX_LENGTH_NUMBER_PLATE = 9
+FOREIGN_CAR_ERROR = 'Вы не можете добавить, изменить или удалить чужой автомобиль.'
+LAST_ONE_CAR = 'Последний автомобиль нельзя удалить. Обратитесь к администратору.'
 
 
 async def check_car_exists(car_id: int, session: AsyncSession) -> None:
-    if await cars_crud.get(car_id, session) is None:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail=CAR_NOT_FOUND
-        )
+    if not await cars_crud.get(car_id, session):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=CAR_NOT_FOUND)
 
 
 async def check_user_exists(telegram_id: str, session: AsyncSession) -> None:
-    if await user_crud.get_user_by_telegram_id(telegram_id, session) is None:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail=USER_NOT_FOUND
-        )
+    if not await user_crud.get_user_by_telegram_id(telegram_id, session):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=USER_NOT_FOUND)
 
 
-async def check_length_user_car_list(
-    telegram_id: str, session: AsyncSession
-) -> None:
+async def check_length_user_car_list(telegram_id: str, session: AsyncSession) -> None:
     if len(await cars_crud.get_my_cars(telegram_id, session)) == 1:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
@@ -57,11 +48,15 @@ async def check_length_user_car_list(
         )
 
 
-async def check_user_is_owner(car: Car, telegram_id: str) -> None:
-    if car.owner_telegram_id != telegram_id:
+async def check_user_is_owner(
+    data: Car, telegram_id: str, session: AsyncSession
+) -> None:
+    await check_user_exists(telegram_id, session)
+    await check_user_exists(data.owner_telegram_id, session)
+    if data.owner_telegram_id != telegram_id:
         raise HTTPException(
             status_code=HTTPStatus.FORBIDDEN,
-            detail=FOREIGN_CAR,
+            detail=FOREIGN_CAR_ERROR,
         )
 
 
@@ -75,71 +70,60 @@ async def check_user_is_admin_or_superuser(
         )
 
 
-async def check_user_before_car_edit(
-    car: Car, telegram_id: str, session: AsyncSession
-) -> None:
-    if not await check_user_is_admin_or_superuser(telegram_id, session):
-        await check_user_is_owner(car, telegram_id)
-
-
-async def check_user_before_car_delete(
-    car: Car, telegram_id: str, session: AsyncSession
-) -> None:
-    if not await check_user_is_admin_or_superuser(telegram_id, session):
-        await check_user_is_owner(car, telegram_id)
-        await check_length_user_car_list(telegram_id, session)
-
-
-async def check_number_plate_duplicate(
-    number_plate, session: AsyncSession
-) -> None:
-    if await cars_crud.get_car_by_number(number_plate, session):
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail=CAR_WITH_NUMBER_EXISTS
-        )
-
-
 async def check_car_data_before_create(
-    telegram_id: str, car_data: CarCreate, session: AsyncSession
+    car_data: CarCreate, session: AsyncSession
 ) -> None:
-    await check_user_exists(car_data.owner_telegram_id, session)
-    if telegram_id != car_data.owner_telegram_id:
-        await check_user_is_admin_or_superuser(telegram_id, session)
-    if len(car_data.brand) < MIN_LENGTH_STR:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail=ERROR_LENGTH_BRAND_MODEL
-        )
-    if len(car_data.model) < MIN_LENGTH_STR:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail=ERROR_LENGTH_BRAND_MODEL
-        )
-    if car_data.number_plate is not None:
-        validate_number_plate(car_data.number_plate)
-        await check_number_plate_duplicate(
-            (car_data.number_plate).upper(), session
-        )
+    await validate_car_fields(car_data)
+    if car_data.number_plate:
+        await validate_number_plate(car_data.number_plate)
+        await check_number_plate_duplicate((car_data.number_plate).upper(), session)
 
 
 async def check_car_before_edit(
     car_id: int, update_data: CarUpdate, session: AsyncSession
 ) -> None:
     car_to_edit = await cars_crud.get(car_id, session)
-    if update_data.number_plate is not None:
-        validate_number_plate(update_data.number_plate)
-        number_plate = (car_to_edit.number_plate).upper()
+    await validate_car_fields(update_data)
+    if update_data.number_plate:
+        await validate_number_plate(update_data.number_plate)
+        number_plate = (update_data.number_plate).upper()
         if number_plate != car_to_edit.number_plate:
             await check_number_plate_duplicate(number_plate, session)
 
 
-# Для ручки добавления/изменения автомобиля для получения текста ошибки на фронтенд
-def validate_number_plate(number_plate) -> None:
+async def check_number_plate_duplicate(number_plate, session: AsyncSession) -> None:
+    if await cars_crud.get_car_by_number(number_plate, session):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail=CAR_WITH_NUMBER_EXISTS
+        )
+
+
+async def validate_car_fields(data) -> None:
+    await validate_field(data, 'brand')
+    await validate_field(data, 'model')
+
+
+async def validate_field(data, field: str) -> None:
+    value = getattr(data, field)
+    if value:
+        if len(value) < MIN_LENGTH_STR:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST, detail=MIN_LENGTH_BRAND_MODEL_ERROR
+            )
+        if len(value) > MAX_LENGTH_BRAND_MODEL:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST, detail=MAX_LENGTH_BRAND_MODEL_ERROR
+            )
+
+
+async def validate_number_plate(number_plate) -> None:
     if (
         len(number_plate) < MIN_LENGTH_NUMBER_PLATE
         or len(number_plate) > MAX_LENGTH_NUMBER_PLATE
     ):
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
-            detail=LENGTH_NUMBER_PLATE,
+            detail=LENGTH_NUMBER_PLATE_ERROR,
         )
     if not re.match(
         f'[{ALLOWED_CHARS}]{{1}}\\d{{3}}[{ALLOWED_CHARS}]{{2}}\\d{{2,3}}',

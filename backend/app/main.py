faaf736dbe778ef1614a1ff3be25e1c0ef472dc3
Elena Lambert <elena_lambert@yahoo.com>
2024-04-06 12:49:54 +0300

    Remove middleware for https

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI(title=settings.app_title)
-app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(HTTPSRedirectMiddleware)
 
 
 @app.get("/")

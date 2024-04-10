4dc36fb9122dd302e5ca616fddb6d77c91deab26
Elena Lambert <elena_lambert@yahoo.com>
2024-04-10 15:49:55 +0300

    Remove test line

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,9 +8,5 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI(title=settings.app_title)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Проверка сервера"}
-
 app.include_router(main_router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")

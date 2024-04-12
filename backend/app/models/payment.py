f5ccd2a7e28203bfc9afe4e05aadf2fb1db0d572
Никита Корняков <nonamebroski@yandex.ru>
2024-04-13 00:07:45 +0300

    Create Payment model.

---
+++ backend/app/models/payment.py
@@ -0,0 +1,47 @@
+import enum
+from datetime import datetime
+
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Integer,
+    String,
+)
+
+from app.core.db import Base
+from app.core.config import MAX_LENGTH_UUID4
+
+
+class PaymentMethod(str, enum.Enum):
+    online = 'online'
+    cash = 'cash'
+
+
+class Payment(Base):
+    generated_payment_id: str = Column(
+        String(MAX_LENGTH_UUID4),
+        nullable=False,
+        unique=True,
+    )
+    date: datetime = Column(DateTime, nullable=False, default=datetime.now)
+    price: int = Column(Integer, nullable=False)
+    payment_method = Column(Enum(PaymentMethod), nullable=False)
+    admin_id: int = Column(
+        Integer,
+        ForeignKey(
+            'user.id',
+            name='fk_payment_admin_id_user',
+        ),
+    )
+    payer_id: int = Column(
+        Integer,
+        ForeignKey(
+            'user.id',
+            name='fk_payment_payer_id_user',
+        ),
+    )
+    is_paid: bool = Column(Boolean, nullable=False, default=False)

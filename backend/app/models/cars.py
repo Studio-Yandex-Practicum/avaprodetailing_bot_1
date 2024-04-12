f99a4303721419dd526af3044f7a7ed4b5999afc
Никита Корняков <nonamebroski@yandex.ru>
2024-04-12 18:20:42 +0300

    Change models with adding History models to all.

--- backend/app/models/cars.py
+++ backend/app/models/cars.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 from app.core.config import MAX_LENGTH_BRAND_MODEL, MAX_LENGTH_NUMBER_PLATE
 from app.core.db import Base
@@ -9,6 +10,9 @@ class Car(Base):
     model: str = Column(String(MAX_LENGTH_BRAND_MODEL), nullable=False)
     number_plate: str = Column(
         String(MAX_LENGTH_NUMBER_PLATE), nullable=False, unique=True
+    )
+    changes = relationship(
+        'CarHistory', backref='car', cascade='all, delete-orphan'
     )
 
     def __repr__(self):
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, Date, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.core.config import (
     MAX_LENGTH_LAST_NAME,
     MAX_LENGTH_PHONE,
-    MAX_LENGTH_USER_INFO_FIELDS
+    MAX_LENGTH_USER_INFO_FIELDS,
 )
 from app.core.db import Base
 
@@ -15,9 +16,20 @@ class User(Base):
     first_name = Column(String(MAX_LENGTH_USER_INFO_FIELDS))
     second_name = Column(String(MAX_LENGTH_USER_INFO_FIELDS))
     last_name = Column(String(MAX_LENGTH_LAST_NAME))
-    birth_date = Column(DateTime)
+    birth_date = Column(Date)
     is_admin = Column(Boolean, default=False)
     is_superuser = Column(Boolean, default=False)
+    car_history = relationship('CarHistory', backref='changed_by')
+    changes = relationship(
+        'UserHistory',
+        foreign_keys='UserHistory.object_id',
+        backref='user',
+    )
+    loyality_history = relationship(
+        'LoyalityHistory',
+        foreign_keys='LoyalityHistory.user_id',
+        backref='user',
+    )
 
     def __repr__(self):
         return (

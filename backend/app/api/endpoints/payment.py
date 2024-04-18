7764f6cb44fdd4a1c63c4bf32a8f4e5905a4ec17
Никита Корняков <nonamebroski@yandex.ru>
2024-04-18 23:48:54 +0300

    Impove code of creating payment.

--- backend/app/api/endpoints/payment.py
+++ backend/app/api/endpoints/payment.py
@@ -6,9 +6,10 @@ from fastapi import APIRouter, BackgroundTasks, Depends, Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.utils import cash_payment_background_tasks
 from app.api.validators import (
     check_admin_user,
-    check_user_exists,
+    check_user_exists_by_phone_number,
 )
 from app.core.db import get_async_session
 from app.crud.payment import payments_crud
@@ -16,6 +17,7 @@ from app.crud.user import user_crud
 from app.schemas.payment import (
     PaymentCashCreate,
     PaymentCreate,
+    PaymentCreateLoyality,
     PaymentDBAdmin,
 )
 from app.services.utils import payment_background_tasks
@@ -56,38 +58,61 @@ async def get_payment(
 
 
 @router.post(
-    '/admin/{admin_telegram_id}/create_payment',
-    response_model=PaymentDBAdmin,
+    '/admin/{admin_telegram_id}/create_payment', response_model=PaymentDBAdmin
 )
 async def add_payment(
-    payment: PaymentCreate,
+    payment: PaymentCreateLoyality,
     admin_telegram_id: str,
     background_tasks: BackgroundTasks,
     session: AsyncSession = Depends(get_async_session),
 ):
     await check_admin_user(admin_telegram_id, session)
-    await check_user_exists(payment.payer_id, session)
-    admin = await user_crud.get_user_by_telegram_id(admin_telegram_id, session)
-    client = await user_crud.get_user_by_telegram_id(payment.payer_id, session)
+    await check_user_exists_by_phone_number(payment.payer_id, session)
+    admin_id = (
+        await user_crud.get_user_by_telegram_id(admin_telegram_id, session)
+    ).id
+    client = await user_crud.get_user_by_phone_number(
+        payment.payer_id, session
+    )
+    payer_id, payer_telegram_id = client.id, client.telegram_id
+    loyality_action = payment.action
+    loyality_amount = payment.loyality_points
+    price = payment.price
+    if loyality_action == 'списано':
+        price = price - loyality_amount
+        loyality_amount = -(loyality_amount)
+        print(loyality_amount)
     match payment.payment_method:
         case 'cash':
             new_payment = await payments_crud.create(
                 PaymentCashCreate(
-                    admin_id=admin.id,
-                    payer_id=client.id,
-                    price=payment.price,
+                    admin_id=admin_id,
+                    payer_id=payer_id,
+                    price=price,
                     payment_method=payment.payment_method,
                     is_paid=True,
                     generated_payment_id=uuid4().__str__(),
                 ),
                 session,
             )
+            background_tasks.add_task(
+                cash_payment_background_tasks,
+                admin_telegram_id,
+                admin_id,
+                payer_telegram_id,
+                payer_id,
+                loyality_action,
+                loyality_amount,
+                new_payment.id,
+                price,
+                session,
+            )
         case 'online':
             new_payment = await payments_crud.create(
                 PaymentCreate(
-                    admin_id=admin.id,
-                    payer_id=client.id,
-                    price=payment.price,
+                    admin_id=admin_id,
+                    payer_id=payer_id,
+                    price=price,
                     payment_method=payment.payment_method,
                 ),
                 session,
@@ -96,7 +121,11 @@ async def add_payment(
                 payment_background_tasks,
                 admin_telegram_id,
                 new_payment,
-                payment.payer_id,
+                payer_telegram_id,
+                admin_id,
+                payer_id,
+                loyality_action,
+                loyality_amount,
                 session,
             )
     return new_payment

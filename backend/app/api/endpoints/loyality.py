7da5cda3726ef1a74dbb99bfa99534689b799ac1
Никита Корняков <nonamebroski@yandex.ru>
2024-04-18 23:50:37 +0300

    Add endpoint of getting points amoun by phone number. Add endpoint of getting loyality form. Impove code of creating loyality.

--- backend/app/api/endpoints/loyality.py
+++ backend/app/api/endpoints/loyality.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, Depends
+from http import HTTPStatus
+
+from fastapi import APIRouter, BackgroundTasks, Depends, Request
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.utils import loyality_background_tasks
 from app.api.validators import (
     check_admin_user,
     check_loyality_balance,
     check_user_exists,
+    check_user_exists_by_phone_number,
 )
 from app.core.config import ID_LOYALITY_SETTINGS
 from app.core.db import get_async_session
@@ -14,10 +19,11 @@ from app.core.descriptions import (
 )
 from app.crud.loyality import loyality_crud, loyality_settings_crud
 from app.crud.user import user_crud
-from app.schemas.loyality import Loyality, LoyalityList, LoyalitySettings
+from app.schemas.loyality import Loyality, LoyalitySettings
 
 
 router = APIRouter()
+templates = Jinja2Templates(directory='app/templates')
 
 
 @router.get('/admin/{telegram_id}/', response_model=LoyalitySettings)
@@ -48,8 +54,26 @@ async def update_loyality_settings(
     )
 
 
+@router.get(
+    '/admin/{admin_telegram_id}/{phone_number}/',
+    description=DESCRIPTION_GET_LOYALITY_POINT,
+)
+async def get_loyality_points_as_admin_by_phone_number(
+    admin_telegram_id,
+    phone_number: str,
+    session: AsyncSession = Depends(get_async_session),
+):
+    await check_admin_user(admin_telegram_id, session)
+    await check_user_exists_by_phone_number(phone_number, session)
+    return dict(
+        count=await loyality_crud.get_count_of_points_by_phone_number(
+            phone_number, session
+        ),
+    )
+
+
 @router.get('/user/{telegram_id}/', description=DESCRIPTION_GET_LOYALITY_POINT)
-async def get_loyality_points(
+async def get_loyality_points_by_telegram_id(
     telegram_id: str, session: AsyncSession = Depends(get_async_session)
 ):
     await check_user_exists(telegram_id, session)
@@ -68,29 +92,53 @@ async def get_loyality_history(
     return await loyality_crud.get_list_of_transactions(telegram_id, session)
 
 
-@router.post('/admin/{telegram_id}/history/{user_telegram_id}/')
-async def add_loyality_history(
+@router.post('/admin/{telegram_id}/')
+async def add_loyality(
     telegram_id: str,
-    user_telegram_id: str,
-    data: LoyalityList,
+    data: Loyality,
+    background_tasks: BackgroundTasks,
     session: AsyncSession = Depends(get_async_session),
 ):
     await check_admin_user(telegram_id, session)
+    user = await user_crud.get_user_by_phone_number(data.user_id, session)
+    user_id = user.id
+    user_telegram_id = user.telegram_id
     await check_user_exists(user_telegram_id, session)
     await check_loyality_balance(data, user_telegram_id, session)
     if data.action == 'списано':
         data.amount = -(data.amount)
+    background_tasks.add_task(
+        loyality_background_tasks,
+        telegram_id,
+        user_telegram_id,
+        data.user_id,
+        data.action,
+        data.amount,
+    )
     return await loyality_crud.create(
         telegram_id,
         user_telegram_id,
         Loyality(
             action=data.action,
             amount=data.amount,
-            user_id=(
-                await user_crud.get_user_by_telegram_id(
-                    user_telegram_id, session
-                )
-            ).id,
+            user_id=user_id,
         ),
         session,
+    )
+
+
+"""FRONTEND"""
+
+
+@router.get('/admin/{admin_telegram_id}/loyality_form')
+async def add_loyality_form(
+    request: Request,
+    admin_telegram_id: str,
+    session: AsyncSession = Depends(get_async_session),
+):
+    await check_admin_user(admin_telegram_id, session)
+    context = {'request': request}
+    context['admin_telegram_id'] = admin_telegram_id
+    return templates.TemplateResponse(
+        'loyality_form.html', context, status_code=HTTPStatus.OK
     )
